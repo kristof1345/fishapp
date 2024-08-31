3570ca02a72ac904d4399cd69ff1c73ab781dd98
kristof1345 <kristofnemeth1345@gmail.com>
2024-08-31 12:13:34 +0200

    added db

--- scripts/maphelpers.js
+++ scripts/maphelpers.js
@@ -1,15 +1,44 @@
-export function saveMarkersToLocalStorage(map, vectorSource) {
+import { supabase } from "./supa.js";
+
+export async function saveMarkersToLocalStorage(map, vectorSource) {
   const features = vectorSource.getFeatures();
   const geojsonFormat = new ol.format.GeoJSON();
   const geojson = geojsonFormat.writeFeatures(features, {
     dataProjection: "EPSG:4326",
     featureProjection: map.getView().getProjection(),
   });
-  localStorage.setItem("map", geojson);
+
+  const { data, error } = await supabase.from("map").select();
+
+  if (!data.length) {
+    const { data, error } = await supabase
+      .from("map")
+      .insert([{ geom: geojson }]);
+    if (error) {
+      console.error(`Error updating record:`, error);
+    }
+  } else {
+    const { data, error } = await supabase
+      .from("map")
+      .update({ geom: geojson })
+      .eq("id", 2);
+    if (error) {
+      console.error(`Error updating record:`, error);
+    }
+  }
+
+  // localStorage.setItem("map", geojson);
 }
 
-export function loadMarkersFromLocalStorage(map, vectorSource) {
-  const geojson = localStorage.getItem("map");
+export async function loadMarkersFromLocalStorage(map, vectorSource) {
+  const { data, error } = await supabase.from("map").select();
+
+  if (error) {
+    console.error(`Error updating record:`, error);
+  }
+
+  const geojson = data[0].geom;
+
   if (geojson) {
     const geojsonFormat = new ol.format.GeoJSON();
     const features = geojsonFormat.readFeatures(geojson, {
---
+++ scripts/supa.js
@@ -0,0 +1,5 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+export const supabase = createClient(
+  "https://ipxjwjvtcwlbxaxdqcin.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlweGp3anZ0Y3dsYnhheGRxY2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwOTY5NTEsImV4cCI6MjA0MDY3Mjk1MX0.6vkHT6zshGODE0ZmnlHYwHQQlA5PGHeir88NEftGHTE"
+);
