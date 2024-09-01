23b1bbb984921388838bc7e4c697d867c5d0a749
kristof1345 <kristofnemeth1345@gmail.com>
2024-09-01 12:02:22 +0200

    toggles

--- scripts/domhelpers.js
+++ scripts/domhelpers.js
@@ -2,8 +2,13 @@
 
 const dropdownBtn = document.getElementById("drop-toggle");
 const dropdownBtn2 = document.getElementById("drop-toggle2");
+const dropdownBtn3 = document.getElementById("drop-toggle3");
 const dropdownDOM = document.getElementById("dropdown");
 const dropdownDOM2 = document.getElementById("dropdown2");
+const dropdownDOM3 = document.getElementById("dropdown3");
+
+const dropdowns = document.getElementsByClassName("dropdown");
+const dropdownToggles = document.getElementsByClassName("drop-toggle");
 
 const popup = document.getElementById("popup");
 
@@ -14,34 +19,46 @@ function toggleSettings() {
 }
 
 function toggleDropdown() {
-  dropdownDOM.classList.toggle("show");
-
-  if (dropdownDOM.classList.contains("show")) {
-    dropdownBtn.innerHTML = `<img src="./assets/exit.svg" alt="Exit menu">`;
+  if (!dropdownDOM.classList.contains("show")) {
+    dropdownDOM.classList.add("show");
+    dropdownBtn.innerHTML = `<img src="./assets/exit.svg" alt="Exit hamburger menu">`;
   } else {
-    dropdownBtn.innerHTML = `<img src="./assets/hamburger.svg" alt="Hamburger menu">`;
-  }
-
-  if (dropdownDOM2.classList.contains("show")) {
-    dropdownDOM2.classList.remove("show");
-    dropdownBtn2.innerHTML = `<img src="./assets/progressiveburger.svg" alt="Hamburger menu">`;
-  }
-}
-
-function toggleDropdown2() {
-  dropdownDOM2.classList.toggle("show");
-
-  if (dropdownDOM2.classList.contains("show")) {
-    dropdownBtn2.innerHTML = `<img src="./assets/exit.svg" alt="Exit menu">`;
-  } else {
-    dropdownBtn2.innerHTML = `<img src="./assets/levels.svg" alt="Hamburger menu">`;
-  }
-
-  if (dropdownDOM.classList.contains("show")) {
     dropdownDOM.classList.remove("show");
     dropdownBtn.innerHTML = `<img src="./assets/hamburger.svg" alt="Hamburger menu">`;
   }
 }
+
+function toggleDropdown2() {
+  if (!dropdownDOM2.classList.contains("show")) {
+    dropdownDOM2.classList.add("show");
+    dropdownBtn2.innerHTML = `<img src="./assets/exit.svg" alt="Exit levels menu">`;
+  } else {
+    dropdownDOM2.classList.remove("show");
+    dropdownBtn2.innerHTML = `<img src="./assets/levels.svg" alt="Levels menu">`;
+  }
+}
+
+function toggleDropdown3() {
+  if (dropdownDOM3.classList.contains("show")) {
+    dropdownDOM3.classList.add("show");
+    dropdownBtn3.innerHTML = `<img src="./assets/exit.svg" alt="Exit eye menu">`;
+  } else {
+    dropdownDOM3.classList.remove("show");
+    dropdownBtn3.innerHTML = `<img src="./assets/eye.svg" alt="Eye icon">`;
+  }
+}
+
+// function unToggleRest(unless) {
+//   let drops = [...dropdowns].filter((drop) => drop != unless);
+//   console.log(drops);
+
+//   drops.map((drop) => {
+//     if (drop.classList.contains("show")) {
+//       drop.classList.remove("show");
+//       console.log(drop.parentNode.children[0].children[0]);
+//     }
+//   });
+// }
 
 document.querySelector(".close").addEventListener("click", (e) => {
   popup.style.display = "none";
